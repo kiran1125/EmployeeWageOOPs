d872156a78c0aa992cef0c0621de6fef37deb698
kiran1125 <makamkiran95@gmail.com>
2021-10-19 22:41:49 +0530

    Added employee Attendance

--- src/com/bridgeLabz/EmpWageOops.java
+++ src/com/bridgeLabz/EmpWageOops.java
@@ -1,7 +1,24 @@
 package com.bridgeLabz;
-
+import java.util.Random;
 public class EmpWageOops {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage Computation");
+        Attendance attendance = new Attendance();
+        attendance.EmployeeCheck();
+    }
+}
+class Attendance{
+    final int IS_PRESENT = 1;
+
+    int GetEmployeePresent(int a) {
+        Random r = new Random();
+        int empCheck = r.nextInt(a);
+        return empCheck;
+    }
+     void EmployeeCheck() {
+        int empCheck = GetEmployeePresent(2);
+        if (empCheck == IS_PRESENT) {
+            System.out.println("Employee is present");
+        } else
+            System.out.println("Employee is Absent");
     }
 }
