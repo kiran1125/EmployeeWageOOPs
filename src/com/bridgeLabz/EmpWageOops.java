e2c43d3467b7cb4fa99f5b6bc2deac75056ac7c7
kiran1125 <makamkiran95@gmail.com>
2021-10-20 06:19:16 +0530

    Added Daily Wage Calculation

--- src/com/bridgeLabz/EmpWageOops.java
+++ src/com/bridgeLabz/EmpWageOops.java
@@ -7,7 +7,10 @@ public class EmpWageOops {
     }
 }
 class Attendance{
-    final int IS_PRESENT = 1;
+    static final int IS_PRESENT = 1;
+    static final int WAGE_PER_HOUR = 20;
+    static final int IS_FULL_TIME = 8;
+    int dailyWage = 0;
 
     int GetEmployeePresent(int a) {
         Random r = new Random();
@@ -18,7 +21,13 @@ class Attendance{
         int empCheck = GetEmployeePresent(2);
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is present");
-        } else
+            dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+
+        }
+        else {
             System.out.println("Employee is Absent");
+            dailyWage = dailyWage;
+        }
+        System.out.println("Daily Wage : " + dailyWage);
     }
 }
