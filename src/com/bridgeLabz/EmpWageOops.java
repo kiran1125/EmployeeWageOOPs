a90b873a7e24005d69e3226a1b6762673620b1f6
kiran1125 <makamkiran95@gmail.com>
2021-10-20 06:30:56 +0530

    Added PartTime to the program

--- src/com/bridgeLabz/EmpWageOops.java
+++ src/com/bridgeLabz/EmpWageOops.java
@@ -8,8 +8,10 @@ public class EmpWageOops {
 }
 class Attendance{
     static final int IS_PRESENT = 1;
+    static final int IS_ABSENT = 0;
     static final int WAGE_PER_HOUR = 20;
     static final int IS_FULL_TIME = 8;
+    static final int IS_PART_TIME = 4;
     int dailyWage = 0;
 
     int GetEmployeePresent(int a) {
@@ -18,15 +20,19 @@ class Attendance{
         return empCheck;
     }
      void EmployeeCheck() {
-        int empCheck = GetEmployeePresent(2);
+        int empCheck = GetEmployeePresent(3);
         if (empCheck == IS_PRESENT) {
             System.out.println("Employee is present");
             dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
 
         }
-        else {
+        else if (empCheck == IS_ABSENT){
             System.out.println("Employee is Absent");
             dailyWage = dailyWage;
+        }
+        else {
+           System.out.println("Employee is Part time");
+           dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
         }
         System.out.println("Daily Wage : " + dailyWage);
     }
