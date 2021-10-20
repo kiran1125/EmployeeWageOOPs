6a0f124d4d3e52d408c628de94dd4adf5e5835fe
kiran1125 <makamkiran95@gmail.com>
2021-10-20 07:40:28 +0530

    Added calculation of Employee Wage and total hours

--- src/com/bridgeLabz/EmpWageOops.java
+++ src/com/bridgeLabz/EmpWageOops.java
@@ -3,8 +3,8 @@ import java.util.Random;
 public class EmpWageOops {
     public static void main(String[] args) {
         Attendance attendance = new Attendance();
-        //attendance.EmployeeCheck();
-        System.out.println(attendance.EmployeeCheck());
+        attendance.EmployeeCheck();
+        //System.out.println(attendance.EmployeeCheck());
     }
 }
 class Attendance{
@@ -16,18 +16,22 @@ class Attendance{
     static final int NO_OF_WORKING_DAYS=20;
     int dailyWage = 0;
     int monthlyWage = 0;
+    int totalHours =0;
+    int hours = 0;
 
     int GetEmployeePresent(int a) {
         Random r = new Random();
         int empCheck = r.nextInt(a);
         return empCheck;
     }
-     int EmployeeCheck() {
+     void EmployeeCheck() {
+        for (int i=0;i<=NO_OF_WORKING_DAYS && totalHours<=100;i++){
         int empCheck = GetEmployeePresent(3);
         switch (empCheck) {
             case IS_PRESENT: {
                 //System.out.println("Employee is present");
                 dailyWage = WAGE_PER_HOUR * IS_FULL_TIME;
+                hours += IS_FULL_TIME;
                 break;
 
             }
@@ -39,11 +43,13 @@ class Attendance{
             default: {
                 //System.out.println("Employee is Part time");
                 dailyWage = WAGE_PER_HOUR * IS_PART_TIME;
+                hours += IS_PART_TIME;
                 break;
             }
-
+        }
         }
         monthlyWage = NO_OF_WORKING_DAYS * dailyWage;
-        return monthlyWage;
+        totalHours = hours;
+        System.out.println("Monthly salary : " +monthlyWage + " Total Hours : " +hours);
     }
 }
